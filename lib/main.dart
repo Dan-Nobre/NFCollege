126fa458a19cb02d93008c7eda30162286806dd5
gilangos <se.gilasss@gmail.com>
2025-02-04 19:36:45 -0300

    feat: add assets (imgs,fonts)

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:nfcollege/Features/InitialScreen/View/InitialScreen.dart';
 
+
 void main() {
   runApp(const MeuApp());
 }
@@ -14,9 +15,14 @@ class MeuApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Meu App Flutter',
       theme: ThemeData(
+        fontFamily: 'Satoshi',
+        textTheme: TextTheme(
+          bodyLarge: TextStyle(fontSize: 18, fontWeight: FontWeight.bold), // Padrão: Negrito
+          bodyMedium: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+          titleLarge: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),),
         primarySwatch: Colors.blue,
       ),
-      home: const InitialScreen(),
+      home:  const InitialScreen(),
     );
   }
 }
